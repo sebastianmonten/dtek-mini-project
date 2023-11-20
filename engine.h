1d5244c3af85016b3403bc011332dd05e1f25119
sebastianmonten <94540990+sebastianmonten@users.noreply.github>
2023-11-20 15:03:01 +0100

    Revert "Merge branch 'main' of github.com:sebastianmonten/dtek-mini-project"
    This reverts commit b7408bcc348425b7807a84c859432513798661ec, reversing
    changes made to d231f80713a67675f8d179a4aa86ca4b7480da19.
    
    going back to working file

--- engine.h
+++ engine.h
@@ -1,8 +1,4 @@
 #ifndef ENGINE_H
 #define ENGINE_H
-#include "graphics.h"
-#include "random.h"
-// Preferrably called every frame.
-// Will also call update_graphics() from graphics.h.
 void update(int ms);
 #endif
