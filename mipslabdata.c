282ade240117bfee03b309c9fa1a4bfa9920f960
mouanga <mouanga@kth.se>
2023-11-23 14:19:53 +0100

    change include

--- mipslabdata.c
+++ mipslabdata.c
@@ -7,7 +7,7 @@
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
-#include "sprite.h"
+#include "object.h"
 #include "mipslabdata.h" // test by me now
 
 char textbuffer[4][16];
